0b1521d8c70554db28127990a5dc14e3a2a2dd44
Rohit patil <136801508+Rohitpatil34@users.noreply.github.com>
2025-09-14 18:06:44 +0530

    Update UpdatePasswordPage.jsx

--- Frontend/src/pages/UpdatePasswordPage.jsx
+++ Frontend/src/pages/UpdatePasswordPage.jsx
@@ -56,7 +56,7 @@ const UpdatePasswordPage = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-100 via-white to-indigo-200 px-4">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-200  px-4 sm:px-6 lg:px-8 pt-20 md:pt-20">
       <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8">
         <h2 className="text-3xl font-extrabold text-center text-gray-800 mb-6">
           Update Password 
